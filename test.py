926e4223ab418bcc5fbb8eddb60840a14be476e0
Minghao Xu <violiniselegant@gmail.com>
2016-06-03 11:27:26 +0800

    command /start googd

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 #!/usr/bin/env python
 from telegram import Bot
-
+from telegram.ext import Updater, CommandHandler
 
 TOKEN = '192123489:AAFzhC6qzSzbii-FVaDxxzWuKENIANzzH7U'
 
@@ -15,12 +15,27 @@ def originBot():
     for u in updates:
         echoMsg(bot, u)
 
+def anotherUpdaterDemo():
+    updater = Updater(token = TOKEN)
+    dispatcher = updater.dispatcher
+
+    start_cmd_handler = CommandHandler('start', echoMsg)
+    dispatcher.add_handler(start_cmd_handler)
+
+    updater.start_polling()
+
 def main():
-    originBot()
+    # originBot()
+    anotherUpdaterDemo()
 
 def echoMsg(bot, update):
+    print 'receive command /start'
+    # print 'receive update content: '+update
+    print update
+    print '----------------begin echo--------------------'
     bot.sendMessage(chat_id=update.message.chat.id,text="echo....")
     bot.sendMessage(chat_id=update.message.chat.id,text="echo:"+update.message.text)
+    print '----------------echo success------------------'
 
 if __name__ == '__main__':
     main()
